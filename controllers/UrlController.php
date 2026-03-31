1f74ad4cdbe4f0445d04ce7501498bbcd8f09926
GTorres <gtorresbotello@gmail.com>
2026-03-31 12:43:45 -0600

    fix url duplicated

--- controllers/UrlController.php
+++ controllers/UrlController.php
@@ -1,27 +1,30 @@
 <?php
 
-require_once __DIR__ . '/../models/Database.php';
+// require_once __DIR__ . '/../models/Database.php';
 
 class UrlController {
     public function add($method)
     {
-        if ($method === 'GET'){            
-            ob_start();
-            include __DIR__ . '/../views/add_url.php';
-            $content = ob_get_clean();
-            include __DIR__ . '/../views/base.php';
-            
-         }elseif ($method === 'POST'){
+        if ($method === 'POST'){
              $pdo = Database::getInstance();
              $original = $_POST['original'];
-             $short = $_POST['short'];
+             $short = strtolower($_POST['short']);
              $short = str_replace(' ', '_', $short);
+             $short = preg_replace('/[^a-z0-9_\-]/', '', $short);
              $short = rawurlencode($short);
-             
-             $stmt = $pdo->prepare("INSERT INTO urls (original_url, short_url, user_id) VALUES (?, ?, ?)");
-             $stmt->execute([$original, $short, $_SESSION['user_id'] ?? null]);
-             header("location: /");
-             exit;
+
+             try {
+                 $stmt = $pdo->prepare("INSERT INTO urls (original_url, short_url, user_id) VALUES (?, ?, ?)");
+                 $stmt->execute([$original, $short, $_SESSION['user_id'] ?? null]);
+                 header("location: /");
+                 exit;
+             } catch (PDOException $e){
+                 if ($e->getCode() == 23000){
+                     $_SESSION['error_msg'] = 'This Url already exist';
+                     header('location: /');
+                     exit;
+                 }
+             }
          }
     }
 }
